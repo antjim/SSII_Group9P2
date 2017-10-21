3b21d5a73870eb3ccabb180bbeadaa1d9cd3a0ec
antjim <ant.jim.v@gmail.com>
2017-10-21 16:46:22 +0200

    logs de incidendias, kpi y modificación inicial del servidor

--- src/servidor/Servidor.java
+++ src/servidor/Servidor.java
@@ -2,7 +2,7 @@ package servidor;
 
 public class Servidor {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		iniciaServidor server =	new	iniciaServidor();	
 		server.runServer();
--- src/servidor/iniciaServidor.java
+++ src/servidor/iniciaServidor.java
@@ -8,6 +8,10 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.security.SecureRandom;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import javax.net.ServerSocketFactory;
@@ -16,18 +20,35 @@ public class iniciaServidor {
 	
 		private	ServerSocket serverSocket;
 		
-		public void IntegrityVerifierServer() throws	Exception {				
+		public iniciaServidor() throws	Exception {				
 			ServerSocketFactory	socketFactory = (ServerSocketFactory) ServerSocketFactory.getDefault();		
 			serverSocket=(ServerSocket)socketFactory.createServerSocket(7070);				
 		}		
 		
 		void runServer() {
+			DateFormat diario=new SimpleDateFormat("12");
+			DateFormat hourFormat;
+			Date date=new Date();
+			List<SecureRandom>contieneR=new ArrayList<SecureRandom>();
+			
+			Boolean completaD=true;
+			Integer success=0;	Integer fails=0;  Integer contKPID=0;
 			
 			while(true) {
-				try	{	
-					//vamos a generar y comprobar los tokens ----
+				try	{
+					hourFormat=new SimpleDateFormat("HH");
+					
+					if(Integer.parseInt(hourFormat.format(date)) > Integer.parseInt(diario.format(date))) {
+						completaD=true;
+					}	//reseteo diario del KPI para no generar más de 1 al día.
+					
+					//tratamiento de tokens----
 					SecureRandom random=Tokens.generaToken();
 					
+					while(contieneR.contains(random))
+						random=Tokens.generaToken();
+					
+					contieneR.add(random);
 					//-------
 					System.err.println(	"Esperando	conexiones	de	clientes...");	
 					Socket socket =	(Socket)serverSocket.accept();
@@ -49,9 +70,21 @@ public class iniciaServidor {
 					
 					if	(macdelMensajeEnviado.equals(macdelMensajeCalculado))	{	
 							output.println("Mensaje enviado integro.");	
+							success++;
 					}else{	
-						output.println(	"Mensaje enviado no integro.");	
+						output.println(	"Mensaje enviado no integro.");
+						fails++;
+						escribirFichero.escribeFallo(mensaje);
 					}
+					
+					//KPI rutinario ----
+					if( diario.equals(hourFormat) && completaD ) {
+						Integer total=success+fails;
+						contKPID++;
+						Kpi.calculaKPI(success, total,contKPID);
+						completaD=false;
+					}
+					// -----------------
 					
 					output.close();			
 					input.close();			
